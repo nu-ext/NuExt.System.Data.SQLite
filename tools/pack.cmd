b5f6ac58e012f0d621fa678f14126b02a19fc4e2
ivanvoyager <5630565+ivanvoyager@users.noreply.github.com>
2026-06-27 16:23:06 +0300

    Improve build & pack

---
+++ tools/pack.cmd
@@ -0,0 +1,3 @@
+@echo off
+powershell -ExecutionPolicy Bypass -File "%~dp0pack.ps1"
+pause
---
+++ tools/pack.ps1
@@ -0,0 +1,7 @@
+$csproj = "$PSScriptRoot\..\src\NuExt.System.Data.SQLite.csproj"
+$Configuration = "Release"
+$outDir = $PSScriptRoot
+
+dotnet clean $csproj -c $Configuration
+dotnet build $csproj -c $Configuration
+dotnet pack $csproj -c $Configuration -o $outDir
