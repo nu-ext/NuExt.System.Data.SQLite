5c69d477190ee5726b1b8c8ed166af49e45b2de2
ivanvoyager <5630565+ivanvoyager@users.noreply.github.com>
2026-06-29 12:48:29 +0300

    v0.7.4

--- tools/pack.ps1
+++ tools/pack.ps1
@@ -3,5 +3,4 @@ $Configuration = "Release"
 $outDir = $PSScriptRoot
 
 dotnet clean $csproj -c $Configuration
-dotnet build $csproj -c $Configuration
 dotnet pack $csproj -c $Configuration -o $outDir
